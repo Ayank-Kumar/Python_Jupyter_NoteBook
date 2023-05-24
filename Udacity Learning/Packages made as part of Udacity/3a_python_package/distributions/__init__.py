8c5cce21759acbbe639a50e1e9285bec4478da79
Ayank-Kumar <77344547+Ayank-Kumar@users.noreply.github.com>
2023-05-25 02:26:53 +0530

    Add files via upload

---
+++ Udacity Learning/Packages made as part of Udacity/3a_python_package/distributions/__init__.py
@@ -0,0 +1,5 @@
+# the init file gets run when package is imported by a python program
+
+from .Gaussiandistribution import Gaussian
+
+# iske bina bhi kaam kar jata , ye shortcut 
---
+++ Udacity Learning/Packages made as part of Udacity/4a_binomial_package/distributions/__init__.py
@@ -0,0 +1,3 @@
+# TODO: import the Binomial class from the Binomialdistribution module
+from .Gaussiandistribution import Gaussian
+from .Binomialdistribution import Binomial
